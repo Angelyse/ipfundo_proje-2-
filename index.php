6ef001a1d0d1140de34dc4115df8293e147c1c56
Angelyse <123158597+Angelyse@users.noreply.github.com>
2023-09-13 09:36:51 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,178 @@
+
+<?php include('head2.php')?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./bootstrap/jquery.min.js">
+    <link rel="stylesheet" href="./bootstrap/umd/popper.min.js">
+    <link rel="stylesheet" href="./bootstrap/js/bootstrap.min.js">
+                        
+    <script src="./bootstrap/js/bootstrap.min.js"></script>
+    
+    <title>Index.html</title>
+   
+    <link rel="stylesheet" href="C:\Users\POSITIVO BGH\Desktop\Ipfundo\bootstrap\css\bootstrap.min.css">
+    <style>
+        /* Styling for the footer */
+        footer {
+            text-align: center;
+            background-color: #333;
+            color: #fff;
+            padding: 10px;
+        }
+         /* Styling for the header */
+        header {
+            text-align: center;
+            background-color: #333;
+            color: #fff;
+            padding: 10px;
+        }
+    </style>
+</head>
+<body>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card" style="background-color: rgb(14, 15, 15);">
+                
+            </div>
+        </div>
+    </div>
+   
+    <div class="row">
+        
+        <div class="col-md-4">
+            
+
+            <div class="card">
+                <div class="card-title text-center" style="color: rgb(7, 7, 7); background-color: rgb(67, 67, 99);"   >
+                    <h4>Children Services</h4>
+                </div>
+                <div class="card-body">
+                    <a href="Children.html">
+                    <img src="R.png" class="img-responsive" alt="Cinque Terre" style="width: 90%;"height="300">
+                    </a>
+                </div>
+                <div class="card-footer" style="background-color: rgb(37, 83, 37);"class="">
+                    Children's services are essential components of a compassionate and inclusive society, 
+                    helping children reach their full potential and ensuring 
+                    that every child has the opportunity to lead a healthy, happy, and productive life. 
+                    These services aim to provide the necessary support and resources to address the unique needs
+                     and challenges that children may encounter during their formative years.
+
+                </div>
+           
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-title text-center" style="color: rgb(7, 7, 7); background-color: rgb(67, 67, 99);">
+                    <h4>Women Services</h4>
+                </div>
+            </div>
+            <div class="card-body">
+                <a href="Chlidren.html">
+                <img src="pregnant3.png" class="img-responsive" alt="Cinque Terre" style="width: 90%;"height="300">
+            </a>
+        </div>
+            <div class="card-footer" style="background-color: rgb(37, 83, 37);">
+                Women's services play a crucial role in advancing gender equality, 
+                empowering women to reach their full potential, and addressing the 
+                unique challenges they may face throughout their lives. 
+                By providing a supportive and inclusive environment, these services 
+                contribute to the well-being and success of women in society, 
+                fostering a more equitable and inclusive world for all.
+
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-title text-center" style="color: rgb(7, 7, 7); background-color: rgb(67, 67, 99);">
+                    <h4>Health Counsellor Services</h4>
+                </div>
+            </div>
+            <div class="card-body">
+                <a href="Health.html">
+                <img src="health.png" class="img-responsive" alt="Cinque Terre" style="width: 100%;"height="300">
+            </a>
+            </div>
+            <div class="card-footer text-center" style="background-color: rgb(37, 83, 37);">
+                Health counselor services are valuable for individuals seeking to
+                 improve their health and well-being, manage health conditions, 
+                 or navigate complex healthcare systems. 
+                 These professionals empower clients to take an active 
+                 role in their health journey, make informed decisions,
+                 and achieve their health-related goals, 
+                 ultimately leading to a healthier and more fulfilling life.
+
+            </div>
+        </div>
+    </div>
+    <div class="row gy-3">
+        <div class="col-md-8 ">
+            <div class="card">
+                <div class="card-title text-center" style="color: rgb(7, 7, 7); background-color: rgb(67, 67, 99);">
+                    <h4>Malnutrition Services</h4>
+                </div>
+                <div class="card-body">
+                    <a href="Malnutrition.html">
+                    <img src="mal.PNG" class="img-responsive" alt="Cinque Terre" style="width: 100%;"height="300">
+                </a>
+                </div>
+                <div class="card-footer" style="background-color: rgb(37, 83, 37);">
+                    Malnutrition services encompass a crucial range of healthcare and 
+                    support programs aimed at identifying, preventing,
+                     and treating malnutrition in individuals,
+                      particularly among vulnerable populations such as children,
+                       pregnant women, and the elderly. 
+                       Malnutrition services are essential for addressing the serious health 
+                       and developmental consequences of inadequate nutrition.
+                        Here's a comprehensive description of malnutrition services
+                        Malnutrition services play a crucial role in improving the health and well-being of individuals 
+                        and communities by addressing the serious consequences of inadequate nutrition. 
+                       
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-title text-center" style="color: rgb(7, 7, 7); background-color: rgb(67, 67, 99);">
+                    <h4>Fight Obesity Services</h4>
+                </div>
+            </div>
+            <div class="card-body">
+                <a href="Obesite.html">
+                <img src="obesity.PNG" class="img-responsive" alt="Cinque Terre" style="width: 100%;"height="300">
+            </a>
+            </div>
+            <div class="card-footer" style="background-color: rgb(37, 83, 37);">
+                Fight obesity services are essential in combating the obesity epidemic and 
+                its associated health risks, including heart disease, diabetes, 
+                and certain cancers. By addressing the multifaceted aspects of obesity, 
+                these services strive to improve the health and quality of life of individuals 
+                
+
+            </div>
+        </div>
+       
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+        <div class="card" style="background-color: rgb(35, 168, 221);">
+            <footer>
+                &copy; <span id="currentYear"></span> IPFUNDO LIFE SYSTEMS. All Rights Reserved.
+            </footer>
+        
+            <!-- JavaScript to update the current year -->
+            <script>
+                document.getElementById("currentYear").textContent = new Date().getFullYear();
+            </script>
+        </div>
+    </div>
+</div>
+</body>
+</html>
